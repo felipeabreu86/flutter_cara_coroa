3b8028c4d7d6a8246690ca63f04a1ed5373484b3
felipeabreu <felipeabreu.rj@gmail.com>
2020-02-11 13:50:35 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cara_coroa/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
