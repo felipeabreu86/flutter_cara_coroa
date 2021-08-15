cc6170c9e93405c47d321bf4f228b583e6e16f7d
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-15 13:19:30 -0300

    Se aplicado, este commit irá criar o layout da Home

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -8,21 +8,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void _exibirResultado() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "Cara ou Coroa",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
+      backgroundColor: Color(0xff61bd86),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 30),
+                      child: Image.asset("assets/images/logo.png"),
+                    ),
+                    GestureDetector(
+                      onTap: _exibirResultado,
+                      child: Image.asset("assets/images/botao_jogar.png"),
+                    ),
+                  ]),
+            ),
           ),
-        ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
